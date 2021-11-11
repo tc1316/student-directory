bf9989639527f9a2d189aae836e785927fecb5bc
Titus <tituschang@hotmail.co.uk>
2021-11-11 11:35:27 +0000

    Step11 done

--- directory.rb
+++ directory.rb
@@ -36,6 +36,7 @@ end
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
+  puts "3. Save the list to students.csv"
   puts "9. Exit" # 9 because we'll be adding more items  
 end
 
@@ -51,6 +52,8 @@ def process(selection)
       input_students
     when "2"
       show_students
+    when "3"
+      save_students
     when "9"
       exit # this will cause the program to terminate
     else
@@ -63,6 +66,17 @@ def interactive_menu
     print_menu
     process(gets.chomp)
   end
+  
+end
+
+def save_students
+  file = File.open("students.csv", "w")
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
 end
 
 interactive_menu
